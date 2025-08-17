c5c44dbef39d8068995cd5a35f17c29ffc4b7b98
Ben Christel <ben.christel@gmail.com>
2025-08-17 13:27:27 -0700

    Bring in equals function and tests from Taste

---
+++ pkg/equals/src/index.test.ts
@@ -0,0 +1,201 @@
+import {test, expect, is, not} from "@benchristel/taste";
+
+import {equals} from "./index.ts";
+
+test("equals", {
+    "compares primitives"() {
+        expect(equals(1, 1), is, true);
+        expect(equals(1, 2), is, false);
+    },
+
+    "compares nulls"() {
+        expect(equals(null, null), is, true);
+    },
+
+    "compares undefineds"() {
+        expect(equals(undefined, undefined), is, true);
+    },
+
+    "treats null and undefined as different"() {
+        expect(equals(null, undefined), is, false);
+    },
+
+    "compares bigints"() {
+        expect(equals(BigInt("10"), BigInt("99")), is, false);
+        expect(equals(BigInt("10"), BigInt("10")), is, true);
+    },
+
+    "compares bigints to numbers"() {
+        expect(equals(BigInt("10"), 10), is, false);
+    },
+
+    "given empty arrays"() {
+        expect(equals([], []), is, true);
+    },
+
+    "given arrays of different lengths"() {
+        expect(equals([], [undefined]), is, false);
+        expect(equals([undefined], []), is, false);
+    },
+
+    "given arrays with different elements"() {
+        expect(equals([1], [2]), is, false);
+    },
+
+    "given equal arrays"() {
+        expect(equals([1, 2, 3], [1, 2, 3]), is, true);
+    },
+
+    "given equal nested arrays"() {
+        expect(equals([1, [2, 3], 4], [1, [2, 3], 4]), is, true);
+    },
+
+    "given unequal nested arrays"() {
+        expect(equals([1, [2, 5], 4], [1, [2, 3], 4]), is, false);
+    },
+
+    "given an array and a non-array"() {
+        expect(equals([], null), is, false);
+        expect(equals(null, []), is, false);
+    },
+
+    "given empty objects"() {
+        expect(equals({}, {}), is, true);
+    },
+
+    "given an array and an object"() {
+        expect(equals([], {}), is, false);
+        expect(equals({}, []), is, false);
+    },
+
+    "given objects with different numbers of keys"() {
+        expect(equals({a: 1}, {}), is, false);
+        expect(equals({}, {a: 1}), is, false);
+    },
+
+    "given objects with different keys"() {
+        expect(equals({a: 1}, {b: 1}), is, false);
+    },
+
+    "given objects with the same keys but different values"() {
+        expect(equals({a: 1}, {a: 2}), is, false);
+    },
+
+    "given equal objects"() {
+        expect(equals({a: 1, b: 2}, {a: 1, b: 2}), is, true);
+    },
+
+    "doesn't care about key order"() {
+        expect(equals({a: 1, b: 2}, {b: 2, a: 1}), is, true);
+    },
+
+    "deep-compares objects"() {
+        expect(equals({a: {b: 1}}, {a: {b: 1}}), is, true);
+    },
+
+    "given classes"() {
+        class ClassOne {}
+        class ClassTwo {}
+        expect(equals(ClassOne, ClassTwo), is, false);
+    },
+
+    "given instances of different classes"() {
+        class ClassOne {}
+        class ClassTwo {}
+        expect(equals(new ClassOne(), new ClassTwo()), is, false);
+    },
+
+    "given instances of the same class with the same properties"() {
+        class ClassOne {
+            public a: number;
+            constructor(a: number) {
+                this.a = a;
+            }
+        }
+        expect(equals(new ClassOne(1), new ClassOne(1)), is, true);
+    },
+
+    "given instances of the same class with different properties"() {
+        class ClassOne {
+            public a: number;
+            constructor(a: number) {
+                this.a = a;
+            }
+        }
+        expect(equals(new ClassOne(1), new ClassOne(2)), is, false);
+    },
+
+    "given a subclass instance and a superclass instance"() {
+        class Superclass {}
+        class Subclass extends Superclass {}
+        expect(equals(new Subclass(), new Superclass()), is, false);
+        expect(equals(new Superclass(), new Subclass()), is, false);
+    },
+
+    "given different functions"() {
+        expect(
+            equals(
+                () => {},
+                () => {},
+            ),
+            is,
+            false,
+        );
+    },
+
+    "given the same function"() {
+        expect(equals(equals, equals), is, true);
+    },
+
+    "given different dates"() {
+        expect(
+            equals(new Date("1999-12-21"), new Date("2001-12-22")),
+            is,
+            false,
+        );
+    },
+
+    "given equal dates"() {
+        expect(
+            equals(new Date("1999-12-21"), new Date("1999-12-21")),
+            is,
+            true,
+        );
+    },
+
+    "given objects with no prototypes"() {
+        expect(equals(Object.create(null), Object.create(null)), is, true);
+    },
+
+    "given different sets with different sizes"() {
+        expect(equals(new Set([1]), new Set([])), is, false);
+        expect(equals(new Set([]), new Set([1])), is, false);
+    },
+
+    "given different sets with different members"() {
+        expect(equals(new Set([1]), new Set([2])), is, false);
+    },
+
+    "given equal sets"() {
+        expect(equals(new Set([1, 2]), new Set([1, 2])), is, true);
+    },
+
+    "given Errors with equal messages"() {
+        expect(equals(new Error("a"), new Error("a")), is, true);
+    },
+
+    "given Errors with different messages"() {
+        expect(equals(new Error("a"), new Error("b")), is, false);
+    },
+
+    "given Errors with different classes"() {
+        class TestError extends Error {}
+        expect(equals(new TestError("a"), new Error("a")), is, false);
+        expect(equals(new Error("a"), new TestError("a")), is, false);
+    },
+
+    "given instances of the same Error subclass"() {
+        class TestError extends Error {}
+        expect(equals(new TestError("a"), new TestError("a")), is, true);
+    },
+});
--- pkg/equals/src/index.ts
+++ pkg/equals/src/index.ts
@@ -2,12 +2,51 @@
  * @module equals
  */
 
-type Equatable = boolean;
-
 /**
- * Deeply compares two values, returning a boolean indicating whether they're
- * equal.
+ * Deeply compares two values, returning true if they're equal and false
+ * otherwise. The following criteria are used to determine equality:
+ * 
+ *   - Arrays are equal iff they have the same length and their corresponding
+ *     elements are equal (according to `equals`).
+ *   - Dates are equal iff they serialize to the same ISO string.
+ *   - Sets are equal iff they contain the same elements. Note that set
+ *     elements are _not_ deeply compared.
+ *   - Errors are equal iff they have the same class and message.
+ *   - Other objects are equal if they have the same prototype (e.g. the same
+ *     class) and the same set of enumerable string-keyed properties, and the
+ *     values of their corresponding properties are equal (according to
+ *     `equals`).
  */
-export function equals(a: Equatable, b: Equatable): boolean {
+export function equals(a: unknown, b: unknown): boolean {
+    if (Array.isArray(a) && Array.isArray(b)) {
+        return a.length === b.length && a.every((_, i) => equals(a[i], b[i]));
+    }
+    if (a instanceof Date && b instanceof Date) {
+        return a.toISOString() === b.toISOString();
+    }
+    if (a instanceof Set && b instanceof Set) {
+        return a.size === b.size && [...a.values()].every((v) => b.has(v));
+    }
+    if (a instanceof Error && b instanceof Error) {
+        return (
+            a.message === b.message &&
+            Object.getPrototypeOf(a)?.constructor ===
+                Object.getPrototypeOf(b)?.constructor
+        );
+    }
+    if (isObject(a) && isObject(b)) {
+        const aKeys = Object.keys(a);
+        const bKeys = Object.keys(b);
+        return (
+            aKeys.length === bKeys.length &&
+            aKeys.every((k) => equals(a[k], b[k])) &&
+            Object.getPrototypeOf(a)?.constructor ===
+                Object.getPrototypeOf(b)?.constructor
+        );
+    }
     return a === b;
+}
+
+function isObject(x: unknown): x is Record<string, unknown> {
+    return !!x && typeof x === "object";
 }
