87d7cf90b24025ce264087bab5fd7974af523f07
Ben Christel <ben.christel@gmail.com>
2025-08-19 17:24:52 -0700

    Add typetests for equals

--- pkg/equals/src/api/typetest.ts
+++ pkg/equals/src/api/typetest.ts
@@ -1,6 +1,18 @@
-import {test, expect} from "tstyche";
+import {describe, it, expect} from "tstyche";
 import {equals} from "../index.ts";
 
-test("equals", () => {
-    expect(equals(true, true)).type.toBeAssignableTo<boolean>();
+describe("equals", () => {
+    it("returns a boolean", () => {
+        expect(equals(1, 1)).type.toBeAssignableTo<boolean>();
+    });
+
+    it("has contravariant parameter types and covariant return type", () => {
+        expect(equals).type.toBeAssignableTo<(a: number, b: string) => void>();
+    });
+
+    it("ignores a third argument", () => {
+        expect(equals).type.toBeAssignableTo<
+            (a: number, b: string, c: unknown) => void
+        >();
+    });
 });
