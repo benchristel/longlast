dad52ea51295702267bdf563d3ebffc5c771a6aa
Ben Christel <ben.christel@gmail.com>
2025-08-24 14:01:41 -0700

    Test all ways of calling a curried function

--- pkg/curry/api/spec.ts
+++ pkg/curry/api/spec.ts
@@ -41,4 +41,113 @@ export function behavesLikeCurry(_curry: typeof curry, name: string): void {
             expect(curriedConcat3("a")(1, true), is, "a1true");
         },
     });
+
+    const curriedConcat4 = _curry(
+        (a: string, b: number, c: boolean, d: BigInt) => a + b + c + d,
+    );
+
+    test(`a 4-ary function from ${name}`, {
+        "can be passed all arguments at once"() {
+            expect(curriedConcat4("a", 1, true, 2n), is, "a1true2");
+        },
+
+        "can be passed its arguments one at a time"() {
+            expect(curriedConcat4("a")(1)(true)(2n), is, "a1true2");
+        },
+
+        "can be passed its arguments 3, 1"() {
+            expect(curriedConcat4("a", 1, true)(2n), is, "a1true2");
+        },
+
+        "can be passed its arguments 2, 2"() {
+            expect(curriedConcat4("a", 1)(true, 2n), is, "a1true2");
+        },
+
+        "can be passed its arguments 2, 1, 1"() {
+            expect(curriedConcat4("a", 1)(true)(2n), is, "a1true2");
+        },
+
+        "can be passed its arguments 1, 3"() {
+            expect(curriedConcat4("a")(1, true, 2n), is, "a1true2");
+        },
+
+        "can be passed its arguments 1, 2, 1"() {
+            expect(curriedConcat4("a")(1, true)(2n), is, "a1true2");
+        },
+
+        "can be passed its arguments 1, 1, 2"() {
+            expect(curriedConcat4("a")(1)(true, 2n), is, "a1true2");
+        },
+    });
+
+    const curriedConcat5 = _curry(
+        (a: string, b: number, c: boolean, d: BigInt, e: string) =>
+            a + b + c + d + e,
+    );
+
+    test(`a 5-ary function from ${name}`, {
+        "can be passed all arguments at once"() {
+            expect(curriedConcat5("a", 1, true, 2n, "b"), is, "a1true2b");
+        },
+
+        "can be passed its arguments one at a time"() {
+            expect(curriedConcat5("a")(1)(true)(2n)("b"), is, "a1true2b");
+        },
+
+        "can be passed its arguments 4, 1"() {
+            expect(curriedConcat5("a", 1, true, 2n)("b"), is, "a1true2b");
+        },
+
+        "can be passed its arguments 3, 2"() {
+            expect(curriedConcat5("a", 1, true)(2n, "b"), is, "a1true2b");
+        },
+
+        "can be passed its arguments 3, 1, 1"() {
+            expect(curriedConcat5("a", 1, true)(2n)("b"), is, "a1true2b");
+        },
+
+        "can be passed its arguments 2, 3"() {
+            expect(curriedConcat5("a", 1)(true, 2n, "b"), is, "a1true2b");
+        },
+
+        "can be passed its arguments 2, 2, 1"() {
+            expect(curriedConcat5("a", 1)(true, 2n)("b"), is, "a1true2b");
+        },
+
+        "can be passed its arguments 2, 1, 2"() {
+            expect(curriedConcat5("a", 1)(true)(2n, "b"), is, "a1true2b");
+        },
+
+        "can be passed its arguments 2, 1, 1, 1"() {
+            expect(curriedConcat5("a", 1)(true)(2n)("b"), is, "a1true2b");
+        },
+
+        "can be passed its arguments 1, 4"() {
+            expect(curriedConcat5("a")(1, true, 2n, "b"), is, "a1true2b");
+        },
+
+        "can be passed its arguments 1, 3, 1"() {
+            expect(curriedConcat5("a")(1, true, 2n)("b"), is, "a1true2b");
+        },
+
+        "can be passed its arguments 1, 2, 2"() {
+            expect(curriedConcat5("a")(1, true)(2n, "b"), is, "a1true2b");
+        },
+
+        "can be passed its arguments 1, 2, 1, 1"() {
+            expect(curriedConcat5("a")(1, true)(2n)("b"), is, "a1true2b");
+        },
+
+        "can be passed its arguments 1, 1, 3"() {
+            expect(curriedConcat5("a")(1)(true, 2n, "b"), is, "a1true2b");
+        },
+
+        "can be passed its arguments 1, 1, 2, 1"() {
+            expect(curriedConcat5("a")(1)(true, 2n)("b"), is, "a1true2b");
+        },
+
+        "can be passed its arguments 1, 1, 1, 2"() {
+            expect(curriedConcat5("a")(1)(true)(2n, "b"), is, "a1true2b");
+        },
+    });
 }
