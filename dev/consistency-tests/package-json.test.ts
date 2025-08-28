8aa04e6fb75163889ff167d4b8066259336a283c
Ben Christel <ben.christel@gmail.com>
2025-08-27 17:07:48 -0700

    Add metadata to package.json files

--- dev/consistency-tests/package-json.test.ts
+++ dev/consistency-tests/package-json.test.ts
@@ -30,5 +30,96 @@ for await (const path of glob(join(root, "pkg", "*", "package.json"))) {
         "specifies a package manager version"() {
             expect(packageConfig.packageManager, is, "pnpm@10.14.0");
         },
+
+        "has a description"() {
+            expect(typeof packageConfig.description, is, "string");
+        },
+
+        "links to the homepage"() {
+            // TODO: uncomment this test once we register the domain name
+            // expect(packageConfig.homepage, is, "https://longlast.js.org/");
+        },
+
+        "links to the GitHub repository"() {
+            // TODO: should this use the object form so we can link to the
+            // directory within the repository? See:
+            // https://docs.npmjs.com/cli/v8/configuring-npm/package-json#repository
+            expect(packageConfig.repository, is, "github:benchristel/longlast");
+        },
+
+        "is MIT-licensed"() {
+            expect(packageConfig.license, is, "MIT");
+        },
+
+        "is authored by Ben Christel"() {
+            expect(
+                packageConfig.author,
+                is,
+                "Ben Christel (https://benchristel.com/)",
+            );
+        },
+
+        "declares whether it has side effects"() {
+            expect(typeof packageConfig.sideEffects, is, "boolean");
+        },
+
+        "defines properties in the order they appear in the NPM docs"() {
+            // See: https://docs.npmjs.com/cli/v8/configuring-npm/package-json
+            const order = [
+                "name",
+                "version",
+                "description",
+                "keywords",
+                "homepage",
+                "bugs",
+                "license",
+                "author",
+                "contributors",
+                "funding",
+                "files",
+                "main",
+                "browser",
+                "bin",
+                "man",
+                "directories",
+                "repository",
+                "sideEffects", // Not documented on npmjs.com
+                "packageManager", // Not documented on npmjs.com
+                "imports", // Not documented on npmjs.com
+                "scripts",
+                "config",
+                "dependencies",
+                "devDependencies",
+                "peerDependencies",
+                "peerDependenciesMeta",
+                "bundleDependencies",
+                "optionalDependencies",
+                "overrides",
+                "engines",
+                "os",
+                "cpu",
+                "private",
+                "publishConfig",
+                "workspaces",
+            ];
+
+            expect(Object.keys(packageConfig), appearInSameOrderAs, order);
+        },
     });
+}
+
+// TODO: consider giving this function its own package.
+function appearInSameOrderAs(as: string[], bs: string[]): boolean {
+    const aSet = new Set(as);
+    const bSet = new Set(bs);
+    const intersectionInBOrder = bs.filter((b) => aSet.has(b));
+    const intersectionInAOrder = as.filter((a) => bSet.has(a));
+
+    if (intersectionInBOrder.length !== intersectionInAOrder.length) {
+        throw Error(
+            "appearInSameOrderAs: invariant failed! Intersection arrays are not the same length.",
+        );
+    }
+
+    return intersectionInBOrder.every((b, i) => b === intersectionInAOrder[i]);
 }
