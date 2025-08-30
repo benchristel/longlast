a23d1f34aeb912f069fc1ff1de89d537e6b888cb
Ben Christel <ben.christel@gmail.com>
2025-08-29 20:07:18 -0700

    Document the exports of the symbols package

--- pkg/symbols/src/index.ts
+++ pkg/symbols/src/index.ts
@@ -2,5 +2,17 @@
  * @module symbols
  */
 
+/**
+ * Identifies a method that takes one argument and compares it to `this`,
+ * returning `true` if the argument is equal to `this` and `false` otherwise.
+ */
+
 export const $equals: unique symbol = Symbol();
+
+/**
+ * Identifies a property that stores a human-readable name for this object
+ * (which is usually a function or class). The value of the `$name` property
+ * should be a string.
+ */
+
 export const $name: unique symbol = Symbol();
