6f588ec47d1be951d2f58003605c2d852bf051a6
Ben Christel <ben.christel@gmail.com>
2025-09-01 14:23:54 -0700

    Use tstyche in symbols package tests

--- pkg/symbols/package-tests/typetest.ts
+++ pkg/symbols/package-tests/typetest.ts
@@ -1,13 +1,17 @@
-import {$equals, $boundArguments, $unapplied} from "#@longlast/symbols";
+import {test, expect} from "tstyche";
+import {$boundArguments, $equals, $unapplied} from "#@longlast/symbols";
 
-// @ts-expect-error
-$equals satisfies string;
-$equals satisfies Symbol;
+test("$equals is a unique symbol", () => {
+    expect($equals).type.toBeAssignableTo<Symbol>();
+    expect($equals).type.not.toBeAssignableWith<Symbol>();
+});
 
-// @ts-expect-error
-$boundArguments satisfies string;
-$boundArguments satisfies Symbol;
+test("$boundArguments is a unique symbol", () => {
+    expect($boundArguments).type.toBeAssignableTo<Symbol>();
+    expect($boundArguments).type.not.toBeAssignableWith<Symbol>();
+});
 
-// @ts-expect-error
-$unapplied satisfies string;
-$unapplied satisfies Symbol;
+test("$unapplied is a unique symbol", () => {
+    expect($unapplied).type.toBeAssignableTo<Symbol>();
+    expect($unapplied).type.not.toBeAssignableWith<Symbol>();
+});
