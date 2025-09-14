898c67a1f43028c69e0398f2be6a378ed50853dd
Ben Christel <ben.christel@gmail.com>
2025-09-13 19:10:29 -0700

    Curry: remove metadata maintenance from the reference implementation

--- pkg/curry/alt/reference.ts
+++ pkg/curry/alt/reference.ts
@@ -1,19 +1,7 @@
-import {$boundArguments, $unapplied} from "@longlast/symbols";
-
 export function curry(f: any): any {
-    const curried = addMetadata(f, [], (...args: any[]): any =>
-        args.length < f.length
-            ? addMetadata(curried, args, (...moreArgs: any[]) =>
-                  curried(...args, ...moreArgs),
-              )
-            : f(...args),
-    );
-    return curried;
-}
-
-function addMetadata(original: any, args: any[], f: any): any {
-    f.displayName = original.displayName ?? original.name;
-    f[$boundArguments] = args;
-    f[$unapplied] = original[$unapplied] ?? f;
-    return f;
+    return function curried(...args: any[]): any {
+        return args.length < f.length
+            ? (...moreArgs: any[]) => curried(...args, ...moreArgs)
+            : f(...args);
+    };
 }
