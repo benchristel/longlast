8adae98495628b588436d71e93b25b5d2eb78e89
Ben Christel <ben.christel@gmail.com>
2025-11-27 11:14:53 -0700

    Equals: benchmark against fast-deep-equal

--- pkg/equals/benchmark.ts
+++ pkg/equals/benchmark.ts
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 import {createBenchmark} from "../../dev/scripts/create-benchmark.ts";
 import {equals as publishedEquals} from "#@longlast/equals";
+import fastDeepEqual from "fast-deep-equal/es6/index.js";
 
 const data1 = createData();
 const data2 = createData();
 
 createBenchmark()
     .add("published", () => publishedEquals(data1, data2))
+    .add("fast-deep-equal", () => fastDeepEqual(data1, data2))
     // JSON-stringifying the data and comparing strings isn't even correct, but
     // since it's an approach someone might reach for to solve a particular
     // instance of the general problem, it's worth comparing it to our `equals`
