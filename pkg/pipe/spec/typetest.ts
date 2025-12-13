61b0ea08a39456aabc1f6b0401aa31ee2b938e71
Ben Christel <ben.christel@gmail.com>
2025-12-13 13:08:40 -0700

    Add ternary pipe

--- pkg/pipe/spec/typetest.ts
+++ pkg/pipe/spec/typetest.ts
@@ -4,8 +4,13 @@ import {expect} from "tstyche";
 const numberToString = (x: number) => String(x);
 const stringToNumber = (s: string) => +s;
 
+expect(pipe).type.not.toBeCallableWith(stringToNumber, stringToNumber);
+
 const roundTripNumber = pipe(numberToString, stringToNumber);
 
 expect(roundTripNumber).type.toBeCallableWith(0);
 expect(roundTripNumber).type.not.toBeCallableWith("");
-expect(pipe).type.not.toBeCallableWith(stringToNumber, stringToNumber);
+
+const fidget = pipe(numberToString, stringToNumber, numberToString);
+
+expect(fidget).type.toBeCallableWith(0);
--- pkg/pipe/src/index.ts
+++ pkg/pipe/src/index.ts
@@ -1,4 +1,13 @@
+export function pipe<A, B, C, D>(
+    f: (a: A) => B,
+    g: (b: B) => C,
+    h: (c: C) => D,
+): (a: A) => D;
 export function pipe<A, B, C>(f: (a: A) => B, g: (b: B) => C): (a: A) => C;
-export function pipe(f: any, g: any): (x: any) => any {
-    return (x) => g(f(x));
+export function pipe(...fs: any): (x: any) => any {
+    return fs.reduceRight(compose);
+}
+
+function compose(f: any, g: any): any {
+    return (x: any) => f(g(x));
 }
