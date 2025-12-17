9e213a2040e2cc01ff730ebbd133cbfaf44a13b0
Ben Christel <ben.christel@gmail.com>
2025-12-17 16:12:35 -0700

    Add TODO

--- pkg/dub/src/index.ts
+++ pkg/dub/src/index.ts
@@ -22,6 +22,12 @@
  */
 
 export function dub<F extends AnyFunction>(name: string, f: F): F {
+    // TODO: Should we set the `name` property here? It's configurable in ES6.
+    // Are there performance concerns with doing that?
+    // Maybe there's no perf problem as long as `dub` is not called by `curry`,
+    // `partialApply`, etc. Do those functions really need to set the name if
+    // they set `$unapplied`?
+    // See: https://github.com/facebook/react/issues/20838
     (f as any).displayName = name;
     return f;
 }
