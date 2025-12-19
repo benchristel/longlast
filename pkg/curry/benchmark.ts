62674003c691f9d1ead0b9aa4fe83d22858f6070
Ben Christel <ben.christel@gmail.com>
2025-12-19 11:35:36 -0700

    Rename function in benchmark

--- pkg/curry/benchmark.ts
+++ pkg/curry/benchmark.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import {createBenchmark} from "../../dev/scripts/create-benchmark.ts";
-import {curry as publishedCurry} from "#@longlast/curry";
+import {curry as longlastCurry} from "#@longlast/curry";
 // @ts-expect-error - mu-lambda has no type declarations.
 import {curry as muLambdaCurry} from "mu-lambda";
 import {curry as ramdaCurry} from "ramda";
@@ -8,13 +8,13 @@ import {default as lodashCurry} from "lodash/fp/curry.js";
 
 const data = new Array(100).fill(0);
 const add = (a: number, b: number) => a + b;
-const addWithPublishedCurry = publishedCurry(add);
+const addWithLonglastCurry = longlastCurry(add);
 const addWithMuLambdaCurry = muLambdaCurry(add);
 const addWithRamdaCurry = ramdaCurry(add);
 const addWithLodashCurry = lodashCurry(add);
 
 createBenchmark()
-    .add("published", () => data.map(addWithPublishedCurry(1)))
+    .add("longlast", () => data.map(addWithLonglastCurry(1)))
     .add("mu-lambda", () => data.map(addWithMuLambdaCurry(1)))
     .add("ramda", () => data.map(addWithRamdaCurry(1)))
     .add("lodash", () => data.map(addWithLodashCurry(1)))
