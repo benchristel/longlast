99738a52d2b80f353dd03c4a2399c9b8fcdbc063
Ben Christel <ben.christel@gmail.com>
2025-12-21 15:06:42 -0700

    Document getFunctionName

--- pkg/function-name/src/index.ts
+++ pkg/function-name/src/index.ts
@@ -1,5 +1,10 @@
 import {type AnyFunction} from "@longlast/any-function";
 
+/**
+ * Returns the name of the given function. If the `displayName`
+ * property is present, it is used in preference to `name`.
+ */
+
 export function getFunctionName(f: AnyFunction): string {
     return (f as any).displayName ?? f.name;
 }
