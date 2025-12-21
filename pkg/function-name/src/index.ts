9d5ff97b7b576108bb9fd6e58e6fb59762299cf6
Ben Christel <ben.christel@gmail.com>
2025-12-21 15:07:28 -0700

    Reflow doc comment

--- pkg/function-name/src/index.ts
+++ pkg/function-name/src/index.ts
@@ -1,8 +1,8 @@
 import {type AnyFunction} from "@longlast/any-function";
 
 /**
- * Returns the name of the given function. If the `displayName`
- * property is present, it is used in preference to `name`.
+ * Returns the name of the given function. If the `displayName` property is
+ * present, it is used in preference to `name`.
  */
 
 export function getFunctionName(f: AnyFunction): string {
