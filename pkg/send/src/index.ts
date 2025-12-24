449b4aaf01a8dc0f8755518b037cb252d4f8cfa9
Ben Christel <ben.christel@gmail.com>
2025-12-24 13:59:47 -0700

    Remove type assertion

--- pkg/send/src/index.ts
+++ pkg/send/src/index.ts
@@ -8,7 +8,6 @@ export function send<M extends string, Args extends any[]>(
 ): <Receiver extends {[Method in M]: (...args: Args) => any}>(
     r: Receiver,
 ) => ReturnType<Receiver[M]> {
-    message satisfies M;
     return <Receiver extends {[Method in M]: (...args: Args) => any}>(
         receiver: Receiver,
     ) => receiver[message](...args);
